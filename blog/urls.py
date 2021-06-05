086ae7b25b660599ddd526aeb12852c2df733da6
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-06-05 22:40:30 +0300

    Fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$',views.views.PostListView.as_view(),name='post_list'),
+    url(r'^$',views.PostListView.as_view(),name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
     url(r'post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
 ]
