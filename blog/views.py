f88810080ce39438ad904831d500e4c18755495f
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-06-10 14:26:46 +0300

    Add PostDeleteView and DraftListView

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 
 from django.shortcuts import render
 from django.views.generic import (TemplateView,ListView,
                                 DetailView,CreateView,
-                                UpdateView)
+                                UpdateView,DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -36,3 +37,15 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
