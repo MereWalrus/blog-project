1f8f0d4818e405a75d038f809dd9b6b6ff672246
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-06-21 19:40:14 +0300

    Make some fixes, register models and migrate

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'',include('blog.urls')),
-    path(r'accounts/login/$',views.login,name='login'),
-    path(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
+    path(r'accounts/login/$',views.LoginView,name='login'),
+    path(r'accounts/logout/$',views.LogoutView,name='logout',kwargs={'next_page':'/'}),
 ]
